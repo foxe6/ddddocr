3fac41279a0f55c26c52f532889d3da3f3283167
f̣ộx̣ệ6 <foxe6@protonmail.com>
2023-03-21 19:18:31 +0800

    Update __init__.py

--- ddddocr/__init__.py
+++ ddddocr/__init__.py
@@ -1446,7 +1446,7 @@ class DdddOcr(object):
                 ('CUDAExecutionProvider', {
                     'device_id': device_id,
                     'arena_extend_strategy': 'kNextPowerOfTwo',
-                    'cuda_mem_limit': 2 * 1024 * 1024 * 1024,
+                    'gpu_mem_limit': 2 * 1024 * 1024 * 1024,
                     'cudnn_conv_algo_search': 'EXHAUSTIVE',
                     'do_copy_in_default_stream': True,
                 }),
