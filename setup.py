1968cf4cc0900d318b41f686e4d6e1366b4c5f35
f̣ộx̣ệ6 <foxe6@protonmail.com>
2023-03-25 08:30:12 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,20 +13,20 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="ddddocr",
-    version="1.4.7",
+    name="ddddocrgpu",
+    version="1.4.8",
     author="sml2h3",
     description="带带弟弟OCR",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/sml2h3/ddddocr",
+    url="https://github.com/foxe6/ddddocrgpu",
     packages=find_packages(where='.', exclude=(), include=('*',)),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['numpy', 'onnxruntime', 'Pillow', 'opencv-python-headless'],
+    install_requires=['numpy', 'onnxruntime-gpu', 'Pillow', 'opencv-python-headless'],
     python_requires='<3.11',
     include_package_data=True,
     install_package_data=True,
