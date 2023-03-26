f30205ed20903a20c3275667a2c9aa6f068c60fd
f̣ộx̣ệ6 <foxe6@protonmail.com>
2023-03-26 08:37:57 +0800

    Create test.py

---
+++ test/slide/test.py
@@ -0,0 +1,5 @@
+import ddddocrgpu
+
+det = ddddocrgpu.DdddOcr(det=False, ocr=False, show_ad=False)
+res = det.slide_match(open('tile.png', 'rb').read(), open('bg.png', 'rb').read(), simple_target=True)
+print(res)
