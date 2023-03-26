bf3e8bc1121e6ad81f631d075236659407608741
f̣ộx̣ệ6 <foxe6@protonmail.com>
2023-03-26 08:51:23 +0800

    Update test.py

--- test/slide/test.py
+++ test/slide/test.py
@@ -1,5 +1,11 @@
 import ddddocrgpu.ddddocrgpu
+from PIL import Image
 
 det = ddddocrgpu.ddddocrgpu.DdddOcr(det=False, ocr=False, show_ad=False)
 res = det.slide_match(open('tile.png', 'rb').read(), open('bg.png', 'rb').read(), simple_target=True)
 print(res)
+x1, y1, x2, y2 = res["target"]
+im = Image.open("bg.png")
+im2 = Image.open("tile.png")
+im.paste(im2, (x1, y1), im2)
+im.save("result.png")
